97e714a991229abd0711fe736e49cda331219224
3MinutesPrivate <chueen.private@gmail.com>
2025-12-03 01:38:01 +0800

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        midnight: "#0F172A",
+        emerald: "#10B981",
+        crimson: "#DC2626"
+      }
+    }
+  },
+  plugins: []
+};
