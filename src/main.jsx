73503ef41167889c72a369ea6c8dc8c43c447646
3MinutesPrivate <chueen.private@gmail.com>
2025-12-03 01:44:47 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import { AppProvider } from "./context/AppContext.jsx";
-import "./styles/index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './styles/index.css';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AppProvider>
-      <App />
-    </AppProvider>
+    <App />
   </React.StrictMode>
 );
