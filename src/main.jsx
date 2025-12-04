f70fa02e47b8c9a7e93ca4deb607de7aa966f1c7
3MinutesPrivate <chueen.private@gmail.com>
2025-12-04 15:32:22 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { AppProvider } from "./context/AppContext.jsx";
+import { AuthProvider } from "./platform/auth/authStore.js";
 import "./styles/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AppProvider>
-      <App />
-    </AppProvider>
+    <AuthProvider>
+      <AppProvider>
+        <App />
+      </AppProvider>
+    </AuthProvider>
   </React.StrictMode>
 );
