3ccf0c1608714898d87583eece7763571fae47d9
3MinutesPrivate <chueen.private@gmail.com>
2025-12-04 15:31:54 +0800

    Create BankerView.jsx

---
+++ src/features/loan_calculator/presentation/views/BankerView.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import LegacyBankerView from "../../../../components/modeC/BankerView.jsx";
+
+function BankerView(props) {
+  return <LegacyBankerView {...props} />;
+}
+
+export default BankerView;
