edd120a063f8e5145f6d133046c01b9cf69573a9
3MinutesPrivate <chueen.private@gmail.com>
2025-12-04 17:30:08 +0800

    Update AgentView.jsx

--- src/features/loan_calculator/presentation/views/AgentView.jsx
+++ src/features/loan_calculator/presentation/views/AgentView.jsx
@@ -3,8 +3,18 @@ import SnapQuote from "../components/agent/SnapQuote.jsx";
 import TrafficLight from "../components/agent/TrafficLight.jsx";
 import DefenseShield from "../components/agent/DefenseShield.jsx";
 import HelperTools from "../components/agent/HelperTools.jsx";
-function AgentView(props) {
-  return <LegacyAgentView {...props} />;
+
+function AgentView() {
+  const [quoteSummary, setQuoteSummary] = React.useState(null);
+
+  return (
+    <div className="space-y-4">
+      <SnapQuote onQuoteChange={setQuoteSummary} />
+      <TrafficLight linkedLoan={quoteSummary} />
+      <DefenseShield />
+      <HelperTools />
+    </div>
+  );
 }
 
 export default AgentView;
