f61cbd9167342c2770ce89843978d608e6326506
3MinutesPrivate <chueen.private@gmail.com>
2025-12-04 15:31:28 +0800

    Create AgentView.jsx

---
+++ src/features/loan_calculator/presentation/views/AgentView.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import LegacyAgentView from "../../../../components/modeB/AgentView.jsx";
+
+function AgentView(props) {
+  return <LegacyAgentView {...props} />;
+}
+
+export default AgentView;
