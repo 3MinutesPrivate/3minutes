241136616c7a83b8da8b46f4cf432318042329c0
3MinutesPrivate <chueen.private@gmail.com>
2025-12-04 17:28:23 +0800

    Update AgentView.jsx

--- src/features/loan_calculator/presentation/views/AgentView.jsx
+++ src/features/loan_calculator/presentation/views/AgentView.jsx
@@ -1,6 +1,8 @@
 import React from "react";
-import LegacyAgentView from "../../../../components/modeB/AgentView.jsx";
-
+import SnapQuote from "../components/agent/SnapQuote.jsx";
+import TrafficLight from "../components/agent/TrafficLight.jsx";
+import DefenseShield from "../components/agent/DefenseShield.jsx";
+import HelperTools from "../components/agent/HelperTools.jsx";
 function AgentView(props) {
   return <LegacyAgentView {...props} />;
 }
